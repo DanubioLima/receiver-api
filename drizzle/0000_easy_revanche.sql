0b4b055396987341be809ddf22322a0d6116bbbc
DanubioLima <danubiovieiralima@gmail.com>
2024-05-12 17:19:59 -0300

    setup tests with docker and github actions

---
+++ drizzle/0000_easy_revanche.sql
@@ -0,0 +1,16 @@
+DO $$ BEGIN
+ CREATE TYPE "public"."pix_key_type" AS ENUM('CPF', 'CNPJ', 'EMAIL', 'TELEFONE', 'CHAVE_ALEATORIA');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "receivers" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"email" varchar(250) NOT NULL,
+	"document" varchar(14) NOT NULL,
+	"pix_key_type" "pix_key_type" NOT NULL,
+	"pix_key" varchar(140) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
