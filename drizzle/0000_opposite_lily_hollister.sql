7b2b9ed752e11afb39cccf6481b79e4dc482bc1b
DanubioLima <danubiovieiralima@gmail.com>
2024-05-14 20:45:05 -0300

    does not allow receivers with same email, pix_key or document

--- drizzle/0000_opposite_lily_hollister.sql
+++ drizzle/0000_opposite_lily_hollister.sql
@@ -13,12 +13,15 @@ END $$;
 CREATE TABLE IF NOT EXISTS "receivers" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
-	"email" varchar(250) NOT NULL,
+	"email" varchar(250),
 	"document" varchar(14) NOT NULL,
 	"status" "receiver_status" DEFAULT 'DRAFT' NOT NULL,
 	"pix_key_type" "pix_key_type" NOT NULL,
 	"pix_key" varchar(140) NOT NULL,
 	"deleted_at" timestamp DEFAULT null,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "receivers_email_unique" UNIQUE("email"),
+	CONSTRAINT "receivers_document_unique" UNIQUE("document"),
+	CONSTRAINT "receivers_pix_key_unique" UNIQUE("pix_key")
 );
